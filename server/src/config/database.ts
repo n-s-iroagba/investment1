51f32c00195837f0c6ed03310152c8b424468e4a
n-s-iroagba <nnamdisolomon1@gmail.com>
2025-05-30 04:17:41 +0100

    hide database.ts

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -1,17 +1,26 @@
-import { Sequelize } from 'sequelize';
-import dotenv from 'dotenv';
+// import { Sequelize } from 'sequelize';
+// import dotenv from 'dotenv';
 
-dotenv.config(); // Load environment variables
+// dotenv.config(); // Load environment variables
 
-const  sequelize =
-(process.env.NODE_ENV === 'production')?
-new Sequelize(process.env.db_string):new Sequelize({
-  dialect: 'mysql',
-  host: 'localhost',
-  username: 'root',
-  password: '97chocho',
-  database: 'iv',
-  logging: false,
-});
+// const sequelize = (process.env.NODE_ENV === 'production') 
+//   ? new Sequelize(process.env.db_string, {
+//       dialect: 'mysql',
+//       pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//       },
+//       logging: false,
+//     })
+//   : new Sequelize({
+//       dialect: 'mysql',
+//       host: 'localhost',
+//       username: 'root',
+//       password: '97chocho',
+//       database: 'iv',
+//       logging: false,
+//     });
 
-export default sequelize;
+// export default sequelize;
